55a4f414d3b3705e6c3b1b57df9a338bbb085c12
Renat Gaynulin <gaynulinrenat@ngs.ru>
2017-04-24 15:59:58 +0700

    Add empty files

---
+++ src/command.h
@@ -0,0 +1,8 @@
+#ifndef
+#define
+
+int encode_file(const char *in_file_name, const char *out_file_name);
+int decode_file(const char *in_file_name, const char *out_file_name);
+
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    // argc - количество аргументов
+    // argv - массив указателей на строки.
+    // Нулевой элемент - командна запуска приложения
+
+    int i;
+    for (i = 0; i < argc; ++i) {
+        printf("argv[%d] = %s\n", i, argv[i]);
+    }
+
+    return 0;
+}
