827d8bf038451c53826e4234978ff4066e823e5d
Renat Gaynulin <gaynulinrenat@ngs.ru>
2017-04-24 19:33:49 +0700

    Add fopen

--- src/main.c
+++ src/main.c
@@ -1,18 +1,33 @@
 #include <stdio.h>
 #include <string.h>
-#include <command.h>
-#inclde "coder.h"
+#include "command.h"
+#inclгde "coder.h"
 
 int main(int argc, char *argv[])
 {
     // argc - количество аргументов
     // argv - массив указателей на строки
     // Нулевой элемент - командна запуска приложения
-
     int i;
     for (i = 0; i < argc; ++i) {
         printf("argv[%d] = %s\n", i, argv[i]);
     }
 
+    FILE *stream;
+    stream = fopen("bin/points.txt", "r");
+        if (stream == NULL) {
+        printf("points.txt cannot be opened");
+        return 1;
+    }
+    fclose(stream):
+
+    FILE *binary;
+    binary = fopen("bin/units.bin", "rb");
+        if (stream == NULL) {
+        printf("units.bin cannot be opened");
+        return 1;
+    }
+    fclose(binary);
+    
     return 0;
 }
