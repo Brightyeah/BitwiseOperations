96c19e7a39deb295992a1b489c27bf29e304a03f
Renat Gaynulin <gaynulinrenat@ngs.ru>
2017-04-25 09:21:50 +0700

    Change command

--- src/command.c
+++ src/command.c
@@ -1,4 +1,5 @@
 #include "command.h"
+#include <inttypes.h>
 
 int encode_file(const char *in_file_name, const char *out_file_name)
 {
@@ -11,27 +12,33 @@ int encode_file(const char *in_file_name, const char *out_file_name)
         return -1;
     }
     stream_out = fopen(out_file_name, "wb");
-        if (stream_out == NULL) {
+    if (stream_out == NULL) {
         return -1;
     }
-    while (fscanf(in, "%" SCNx32, &code_point) == 1) {
+    while (fscanf(in, "%" SCNx32, &code_point) != 0) {
         encode(code_point, code_units);
         write_code_unit(stream_out, code_units);
         code_point = 0;
     }
     fclose(stream_in);
     fclose(stream_out);
+    return 0;
 }
 int decode_file(const char *in_file_name, const char *out_file_name)
 {
-    
     FILE *stream_in, *stream_out;
     stream_in = fopen(in_file_name, "rb");
     if (stream_in == NULL) {
         return -1;
     }
-    stream2 = fopen(out_file_name, "w");
-        if (stream_out == NULL) {
+    stream_out = fopen(out_file_name, "w");
+    if (stream_out == NULL) {
         return -1;
     }
+    while () {
+        
+    }
+    fclose(stream_in);
+    fclose(stream_out);
+    return 0;
 }
--- src/main.c
+++ src/main.c
@@ -13,45 +13,5 @@ int main(int argc, char *argv[])
         printf("argv[%d] = %s\n", i, argv[i]);
     }
 
-    FILE *stream1, *stream2;
-    stream1 = fopen("bin/points.txt", "r");
-    if (stream1 == NULL) {
-        printf("units.bin cannot be opened\n");
-        return 1;
-    }
-    stream2 = fopen("bin/units.bin", "wb");
-        if (stream2 == NULL) {
-        printf("units.txt cannot be opened\n");
-        return 1;
-    }
-    int k = encode_file("bin/points.txt", "bin/units.bin");
-    if (k == 0) {
-        printf("Success\n");
-    } else {
-        printf("Fail\n");
-    }
-    fclose(stream1);
-    fclose(stream2):
-
-    FILE *binary1, *binary2;
-    binary1 = fopen("bin/units.bin", "rb");
-    if (binary1 == NULL) {
-        printf("units.bin cannot be opened\n");
-        return 1;
-    }
-    binary2 = fopen("bin/points.txt", "w");
-    if (binary2 == NULL) {
-        printf("points.bin cannot be opened\n");
-        return 1;
-    }
-    k = decode_file("bin/units.bin", "bin/points.txt");
-    if (k == 0) {
-        printf("Success\n");
-    } else {
-        printf("Fail\n");
-    }
-    fclose(binary1);
-    fclose(binary2);
-    
     return 0;
 }
