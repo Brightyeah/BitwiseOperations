e937d7ce93209a8f7557aa76d755707811d4f00e
Renat Gaynulin <gaynulinrenat@ngs.ru>
2017-04-25 08:49:37 +0700

    Change encode

--- src/command.c
+++ src/command.c
@@ -3,7 +3,8 @@
 int encode_file(const char *in_file_name, const char *out_file_name)
 {
     uint32_t code_point;
-    CodeUnits *code_units
+    CodeUnits units;
+    CodeUnits *code_units = &units;
     FILE *stream_in, *stream_out;
     stream_in = fopen(in_file_name, "r");
     if (stream_in == NULL) {
@@ -15,7 +16,8 @@ int encode_file(const char *in_file_name, const char *out_file_name)
     }
     while (fscanf(in, "%" SCNx32, &code_point) == 1) {
         encode(code_point, code_units);
-        write_code_unit(stream_out, code_unit);
+        write_code_unit(stream_out, code_units);
+        code_point = 0;
     }
     fclose(stream_in);
     fclose(stream_out);
